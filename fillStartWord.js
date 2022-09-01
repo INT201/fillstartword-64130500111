0fc467f9aa5b6bcc106623a96d07316e4bc48774
Jemz <102203151+64130500111@users.noreply.github.com>
2022-09-01 12:45:18 +0700

    if-else add

--- fillStartWord.js
+++ fillStartWord.js
@@ -3,9 +3,16 @@ const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
 
-  console.log(`${startWord}${word}`)
-  
+  if(startWord != null && word != null) {
+    console.log(`${startWord}${word}`)
+    
+  }else {
+    
+    console.log("undefined")
+  }
+
 }
 
+fillStartWord("hello")
 
 module.exports = fillStartWord
