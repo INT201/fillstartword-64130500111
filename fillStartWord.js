8a28ffbdf1a12aa64bc54604f7c45f79d4995642
Jemz <102203151+64130500111@users.noreply.github.com>
2022-09-02 00:52:20 +0700

    update func v2

--- fillStartWord.js
+++ fillStartWord.js
@@ -2,22 +2,23 @@ const { template } = require('@babel/core')
 
 
 function fillStartWord(startWord, word) {
-
-  if(word == null || undefined){
-    return undefined
-  }else if (startWord === word.substr(0,startWord.length)){
-    console.log(word);
+  if(word == null || word == undefined){
+    console.log(undefined)
+  }else if(startWord == word.substr(0, startWord.lenth)){
+    console.log(word)
   }else{
-    console.log(`${startWord}${word}`)
+    console.log(startWord+word)
   }
-
+    
+  
 
 }
 
-fillStartWord('1-2565-', 'Hello World')
-fillStartWord('JS', 'beginner')
-fillStartWord('first', 'firststep')
-fillStartWord('second', undefined)
-fillStartWord('third', null)
+
+// fillStartWord('1-2565-', 'Hello World')
+// fillStartWord('JS', 'beginner')
+// fillStartWord('first', 'firststep')
+// fillStartWord('second', undefined)
+// fillStartWord('third', null)
 
 module.exports = fillStartWord
