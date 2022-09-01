c4f987fb9f7978c3a99364caf9bf3c7d6438b509
Jemz <102203151+64130500111@users.noreply.github.com>
2022-09-02 00:57:59 +0700

    update func again

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,27 +1,13 @@
 const { template } = require('@babel/core')
 
 
-// function fillStartWord(startWord, word) {
-//   if(word == null || word == undefined){
-//     console.log(undefined)
-//   }else if(startWord == word.substr(0, startWord.lenth)){
-//     console.log(word)
-//   }else{
-//     console.log(startWord+word)
-//   }
-// }
 function fillStartWord(startWord, word) {
-  let fullWord;
-  if (word == null||word == undefined) {
-      fullWord = undefined;
-      return fullWord;
-  } else if (startWord === word.substr(0, startWord.length)) {
-      fullWord = word;
-      return fullWord;
-  }
-  else {
-      fullWord = startWord + word;
-      return fullWord;
+  if(word == null || word == undefined){
+    return undefined
+  }else if(startWord == word.substr(0, startWord.lenth)){
+    return word
+  }else{
+    return `${startWord}${word}`
   }
 }
     
@@ -30,10 +16,10 @@ function fillStartWord(startWord, word) {
 
 
 
-fillStartWord('1-2565-', 'Hello World')
-fillStartWord('JS', 'beginner')
-fillStartWord('first', 'firststep')
-fillStartWord('second', undefined)
-fillStartWord('third', null)
+// fillStartWord('1-2565-', 'Hello World')
+// fillStartWord('JS', 'beginner')
+// fillStartWord('first', 'firststep')
+// fillStartWord('second', undefined)
+// fillStartWord('third', null)
 
 module.exports = fillStartWord
