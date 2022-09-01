15a2c99890e6b3ecad143620e1975870447c325d
Jemz <102203151+64130500111@users.noreply.github.com>
2022-09-02 00:59:44 +0700

    update fucn again v2

--- fillStartWord.js
+++ fillStartWord.js
@@ -4,7 +4,7 @@ const { template } = require('@babel/core')
 function fillStartWord(startWord, word) {
   if(word == null || word == undefined){
     return undefined
-  }else if(startWord == word.substr(0, startWord.lenth)){
+  }else if(startWord === word.substr(0, startWord.lenth)){
     return word
   }else{
     return `${startWord}${word}`
